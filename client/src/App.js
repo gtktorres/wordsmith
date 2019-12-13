c0cc5e0035311eca01bbc6454d4242a8c0f88f06
Guevara Torres <guevara.torres619@outlook.com>
2019-12-13 02:06:23 -0500

    Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { Login } from './Login';
+import  Login  from './Login';
+import { handleInput } from './methods';
 
 class App extends React.Component {
-  
-  render() {
-    
-    return (
-      <Login />
-    );
-  }
+
+    constructor() {
+        super();
+        this.state = {
+            userId: "",
+            showLogin: true,
+            isLoading: false,
+            language: 'en',
+        };
+
+        this.handleInput = handleInput.bind(this);
+    }
+
+    render() {
+        
+        const userId = this.state;
+
+        return (
+            
+                <Login
+                  userId={userId}
+                  handleInput={this.handleInput}
+                  connectToChatkit={this.connectToChatkit}
+                />
+        );
+    }
 }
 
 export default App;
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,16 +1,58 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 import "./Login.css";
 
-export class Login extends React.Component {
-    render() {
-        return (
-            <div className="Login">
-                <div className="Border">
-                    <h3 className="Header">Welcome!</h3>
-                    <h3 className="Header">Добро пожаловать!</h3>
-                    <h3 className="Header">欢迎</h3>
+const Login = props => {
+    const { userId, password, handleInput } = props;
+
+    return (
+        <div className="Login">
+            <div className="Border">
+                <h1>Wordsmith</h1>
+                <h3 className="Header">Welcome!</h3>
+                <h3 className="Header">Добро пожаловать!</h3>
+                <h3 className="Header">欢迎</h3>
+
+                <div className="Credentials">
+                    <input
+                        id="userId"
+                        className="username-input"
+                        autoFocus
+                        type="text"
+                        name="userId"
+                        value={userId}
+                        onChange={handleInput}
+                        placeholder="username"
+                    />
+                    <input
+                        id="password"
+                        className="password-input"
+                        autoFocus
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={handleInput}
+                        placeholder="password"
+                    />
+                </div>
+                <div>
+                    <button type="submit" className="submit-btn">
+                        <p>Login</p>
+                    </button>
+                </div>
+                <div className="Create">
+                    <h3 className="Header">Need an account?</h3>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+
 }
+
+Login.propTypes = {
+    userId: Proptypes.string.isRequired,
+    handleInput: Proptypes.func.isRequired,
+    password: Proptypes.string.isRequired,
+};
+
+export default Login;
